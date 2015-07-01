bee050fa7613421ae99cdf745e36fef84cd86a9c
Krzysztof Jeske <krzysztof.jeske@ciber.com>
2015-07-01 10:27:05 +0200

    Code documentation

--- src/knockout.bliss.js
+++ src/knockout.bliss.js
@@ -14,87 +14,83 @@
     var bindingPrefix = '*';
     var regularBindingAttributeName = 'data-bind';
 
-    function parseExistingBindings(val){
+    // Converts object literal described in a string into
+    // a regular literal object
+    function parseBindingsString(bindingValue) {
      
-      var result = { };
+        var result = { };
+        
+        // if the value doesn't start with '{' then it is
+        // a primitive value that we shouldn't parse       
+        if (bindingValue.charAt(0) !== '{'){
+            return bindingValue;
+        }
       
-      if (val.charAt(0) !== '{'){
-        return val;
-      }
-      
-      var array = ko.expressionRewriting.parseObjectLiteral(val);
+        // convert string with bindings to the array by KO
+        var array = ko.expressionRewriting.parseObjectLiteral(bindingValue);
     
-      for (var i = 0; i < array.length; i++) {
-        result[array[i].key] = parseExistingBindings(array[i].value);
-      } 
+        for (var i = 0; i < array.length; i++) {
+            result[array[i].key] = parseBindingsString(array[i].value);
+        } 
     
-      return result;
+        return result;
+    }
+
+    // Converts bindings string described inside the data-bind
+    // attribute into a literal object     
+    function convertBindingsStringToObject(bindings) {
+        if (!bindings){
+            return {};
+        }
+                
+        if (bindings.charAt(0) !== '{'){
+            bindings = '{' + bindings + '}';
+        }
+        
+        return parseBindingsString(bindings);
     }
     
-    function objectLiteralToText(obj) {
+    // Converts object literal to string
+    function objectLiteralToString(obj) {
         var result = '';
     
         if (typeof(obj) === "object" && !obj.join) {
             var list = [];
+            
             for (var property in obj) {
-                list.push("'" + property + "': " + objectLiteralToText(obj[property]));
+                list.push("'" + property + "': " + objectLiteralToString(obj[property]));
             };
             
             result += "{" + list.join(",") + "}";
         } else {
-          result += obj;
+            result += obj;
         }
     
         return result;
+    }   
+    
+    // Retrieves an array of attributes that satisfy
+    // the special binding syntax
+    function getBindingAttributesArray(node){
+
+        var result = [];
+        
+        for (var i = 0; i < node.attributes.length; i++) {
+            var attribute = node.attributes[i];
+            
+            if (attribute.name.substring(0, bindingPrefix.length) !== bindingPrefix) {
+                continue;
+            }
+            
+            result.push(attribute);
+        }
+  
+        return result;        
     }
     
-    var previousPreprocessNode = ko.bindingProvider.instance.preprocessNode;
-    
-    ko.bindingProvider.instance.preprocessNode = function(node) {
-      
-      if (previousPreprocessNode) {
-          previousPreprocessNode(node);
-      }
-      
-      if (node.nodeType != 1 && node.nodeType != 8){
-        return;  
-      }
-    
-      if (!node.hasAttributes || !node.hasAttributes()) {
-        return;    
-      }
-
-      var bindingAttributes = [];
-
-      for (var i = 0; i < node.attributes.length; i++) {
-        var attribute = node.attributes[i];
-        
-        if (attribute.name.substring(0, bindingPrefix.length) !== bindingPrefix) {
-          continue;
-        }
-        
-        bindingAttributes.push(attribute);
-      }
-      
-      if (bindingAttributes.length === 0) {
-          return;
-      }
-      
-      var existingBindings = {};
-      var dataBindAttribute = node.getAttribute(regularBindingAttributeName);
-      
-      if (dataBindAttribute) {
-          
-        if (dataBindAttribute.charAt(0) !== '{'){
-          dataBindAttribute = '{' + dataBindAttribute + '}';
-        }
-        
-        existingBindings = parseExistingBindings(dataBindAttribute);
-      }
-    
-      for (var k = 0; k < bindingAttributes.length; k++) {
-    
-        var attr = bindingAttributes[k];
+    // Appends an attribute hierarchical value into 
+    // a literal object that represents all the bindings 
+    function appendBindingsFromAttribute(existingBindings, attr) {
         
         var pathItems = attr.name
             .substring(bindingPrefix.length)
@@ -102,25 +98,76 @@
             
         var currentBinding = existingBindings;
         
-        for (var p = 0; p < pathItems.length; p++) {
+        for (var i = 0; i < pathItems.length; i++) {
 
-          var pathItem = pathItems[p];
-          
-          if (pathItem.indexOf('-') != -1){
-            pathItem = "'" + pathItem + "'";
-          }
-          
-          var value = currentBinding[pathItem] || {};
-          
-          if (p == pathItems.length - 1) {
-            value = attr.value;
-          }
-          
-          currentBinding[pathItem] = value;               
-          currentBinding = currentBinding[pathItem];
+            var pathItem = pathItems[i];
+            
+            if (pathItem.indexOf('-') != -1){
+                pathItem = "'" + pathItem + "'";
+            }
+            
+            var value = currentBinding[pathItem] || {};
+            
+            if (i == pathItems.length - 1) {
+                value = attr.value;
+            }
+            
+            currentBinding[pathItem] = value;               
+            currentBinding = currentBinding[pathItem];
+        }         
+    }
+    
+    
+    // Validates node whether it should be taken into
+    // consideration when building the bindings list 
+    function validateBindingNode(node){
+        
+        if (node.nodeType != 1 && node.nodeType != 8) {
+            return false;  
+        }
+        
+        if (!node.hasAttributes || !node.hasAttributes()) {
+            return false;    
+        }
+
+        return true;
+    }
+    
+    // Processes a node if it satisfy the validation condition
+    // in order to create the bindings 
+    function preprocessNode(node){
+        
+        if (!validateBindingNode(node)){
+            return;
+        }
+        
+        var bindingAttributes = getBindingAttributesArray(node);
+        
+        if (bindingAttributes.length === 0) {
+            return;
+        }
+        
+        var dataBindAttribute = node.getAttribute(regularBindingAttributeName);
+        var existingBindings = convertBindingsStringToObject(dataBindAttribute);
+
+        for (var i = 0; i < bindingAttributes.length; i++) {
+            appendBindingsFromAttribute(existingBindings, bindingAttributes[i]);        
         } 
-      }
+            
+        node.setAttribute(regularBindingAttributeName, objectLiteralToString(existingBindings));        
+    }
+    
+    // existing implementation of preprocessNode method
+    var previousPreprocessNode = ko.bindingProvider.instance.preprocessNode;
+    
+    ko.bindingProvider.instance.preprocessNode = function(node) {
       
-      node.setAttribute(regularBindingAttributeName, objectLiteralToText(existingBindings));
+        // if there is a existing implementation of
+        // preprocessNode then run it before 
+        if (previousPreprocessNode) {
+            previousPreprocessNode(node);
+        }
+        
+        preprocessNode(node);
     }; 
 }));
